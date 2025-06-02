4ca0f7b10010446a5792914d644eaa92ae739cf5
sweepstouch957 <sweepstouchcloud@gmail.com>
2025-06-02 12:35:09 -0600

    remove dependecy

--- src/components/application-ui/map/index.tsx
+++ src/components/application-ui/map/index.tsx
@@ -1,16 +1,5 @@
 'use client';
 
-import 'leaflet/dist/leaflet.css';
-import L, { Icon } from 'leaflet';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-
-const icon: Icon = new L.Icon({
-  iconUrl: '/marker-icon.png',
-  shadowUrl: '/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-});
-
 type StoreMapProps = {
   lat: number;
   lng: number;
@@ -18,7 +7,5 @@ type StoreMapProps = {
 };
 
 export const StoreMap = ({ lat, lng, name }: StoreMapProps) => {
-  const position: [number, number] = [lat, lng]; // ✅ definir tipo tuple explícito
-
   return <></>;
 };
