a20fe925495e8f5bb6a37b4a0c6a0ca031fbaa7d
Jared Palma <jared@sweepstouch.com>
2025-10-13 14:52:57 -0600

    creado step 2 para el formulario nueva tienda

---
+++ src/app/admin/management/stores/create/step-2/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import CreateStoreStep2 from '@/components/admin/stores/CreateStoreStep2';
+
+export default function Page(): React.JSX.Element {
+  return <CreateStoreStep2 />;
+}
