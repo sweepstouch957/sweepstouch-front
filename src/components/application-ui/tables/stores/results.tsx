c27a010ce93fd8bd7806e850d974cbf8c0211575
sweepstouch957 <sweepstouchcloud@gmail.com>
2026-01-13 08:02:28 -0600

    estimated pay

--- src/components/application-ui/tables/stores/results.tsx
+++ src/components/application-ui/tables/stores/results.tsx
@@ -329,7 +329,7 @@ const Results = ({
                     </TableCell>
 
                     {/* ⭐ COL 3: Weekly Campaign Cost */}
-                    <TableCell align="right">{t('Weekly campaigns')}</TableCell>
+                    <TableCell align="right">{t('Estimated Pay')}</TableCell>
 
                     {/* COL 4: Balance */}
                     <TableCell align="right">{t('Balance')}</TableCell>
