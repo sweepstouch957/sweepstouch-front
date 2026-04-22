91eada24c7ad1e71375054edc9871089c0a1dffe
Allan Castro <71955202+Allan021@users.noreply.github.com>
2026-04-22 00:19:52 -0600

    1

--- src/app/admin/management/projects/page.tsx
+++ src/app/admin/management/projects/page.tsx
@@ -7,7 +7,6 @@ import { useTranslation } from 'react-i18next';
 import ProjectsListing from 'src/components/application-ui/tables/projects/projects';
 import PageHeading from 'src/components/base/page-heading';
 import { useCustomization } from 'src/hooks/use-customization';
-import { Layout } from 'src/layouts';
 
 function Page(): React.JSX.Element {
   const customization = useCustomization();
