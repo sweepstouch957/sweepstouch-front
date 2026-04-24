005ad02c6b63e40582ba598fb7d2dde3bf39785f
Allan Castro <sweepstouchcloud@gmail.com>
2026-04-24 15:48:29 -0600

    asdasd

--- src/app/admin/management/campaign-requests/[id]/page.tsx
+++ src/app/admin/management/campaign-requests/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { use } from 'react';
 import RequestDetail from '@/components/campaign-requests/RequestDetail';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Box, Button, Container } from '@mui/material';
@@ -8,10 +9,11 @@ import PageHeading from 'src/components/base/page-heading';
 import { useCustomization } from 'src/hooks/use-customization';
 
 interface Props {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export default function CampaignRequestDetailPage({ params }: Props) {
+  const { id } = use(params);
   const customization = useCustomization();
   const router = useRouter();
 
@@ -37,7 +39,7 @@ export default function CampaignRequestDetailPage({ params }: Props) {
 
       <Box pb={{ xs: 2, sm: 4 }}>
         <Container maxWidth={customization.stretch ? false : 'xl'}>
-          <RequestDetail id={params.id} />
+          <RequestDetail id={id} />
         </Container>
       </Box>
     </>
